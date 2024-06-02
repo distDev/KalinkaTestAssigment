c3e2d7d76ed9b9bf964f56cbffa4bcb4879f2129
Dmitry Staver <dmitry.staver@mmco-expo.ru>
2024-06-02 16:37:41 +0300

    feat: add start template

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <Header />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+  import Header from '@/components/layout/Header.vue'
+</script>
+
+<style>
+  .container {
+    max-width: 1280px;
+    margin: 0 auto;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default { name: "index" };
+</script>
+
+<script setup></script>
+
+<style scoped></style>
